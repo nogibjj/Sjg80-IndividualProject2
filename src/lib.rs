2ca0e3bcb83e6515a65e1226a5a6aa83e0521b39
shailajgg <142270941+shailajgg@users.noreply.github.com>
2023-10-23 00:12:51 -0400

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,6 @@
 //!
 //! Open a connection, create a table, and insert a few rows:
 //!
-//! ```
 //! let connection = sqlite::open(":memory:").unwrap();
 //!
 //! let query = "
@@ -13,12 +12,10 @@
 //!     INSERT INTO users VALUES ('Bob', 69);
 //! ";
 //! connection.execute(query).unwrap();
-//! ```
 //!
 //! Select some rows and process them one by one as plain text, which is generally
 //! not efficient:
 //!
-//! ```
 //! # let connection = sqlite::open(":memory:").unwrap();
 //! # let query = "
 //! #     CREATE TABLE users (name TEXT, age INTEGER);
@@ -36,12 +33,10 @@
 //!         true
 //!     })
 //!     .unwrap();
-//! ```
 //!
 //! Run the same query but using a prepared statement, which is much more efficient
 //! than the previous technique:
 //!
-//! ```
 //! use sqlite::State;
 //! # let connection = sqlite::open(":memory:").unwrap();
 //! # let query = "
@@ -59,11 +54,9 @@
 //!     println!("name = {}", statement.read::<String, _>("name").unwrap());
 //!     println!("age = {}", statement.read::<i64, _>("age").unwrap());
 //! }
-//! ```
 //!
 //! Run the same query but using a cursor, which is iterable:
 //!
-//! ```
 //! # let connection = sqlite::open(":memory:").unwrap();
 //! # let query = "
 //! #     CREATE TABLE users (name TEXT, age INTEGER);
@@ -85,7 +78,6 @@
 //!     println!("name = {}", row.read::<&str, _>("name"));
 //!     println!("age = {}", row.read::<i64, _>("age"));
 //! }
-//! ```
 //!
 //! [1]: https://www.sqlite.org
 
