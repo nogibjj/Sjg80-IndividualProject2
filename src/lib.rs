69214be1eaa95bf7db5d9aac0c95b18b6f839ec8
shailajgg <142270941+shailajgg@users.noreply.github.com>
2023-10-22 22:26:23 -0400

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -89,6 +89,16 @@
 //!
 //! [1]: https://www.sqlite.org
 
+let connection = sqlite::open(":memory:").unwrap();
+
+let query = "
+    CREATE TABLE users (name TEXT, age INTEGER);
+    INSERT INTO users VALUES ('Alice', 42);
+    INSERT INTO users VALUES ('Bob', 69);
+";
+connection.execute(query).unwrap();
+
+
 extern crate sqlite3_sys as ffi;
 
 macro_rules! c_str_to_str(
