1ed2cf4eef5e8e2dc9b597c0b478d40012252613
shailajgg <142270941+shailajgg@users.noreply.github.com>
2023-10-22 22:25:56 -0400

    Delete src/main.rs

--- src/main.rs
+++
@@ -1,9 +0,0 @@
-let connection = sqlite::open(":memory:").unwrap();
-
-let query = "
-    CREATE TABLE users (name TEXT, age INTEGER);
-    INSERT INTO users VALUES ('Alice', 42);
-    INSERT INTO users VALUES ('Bob', 69);
-";
-connection.execute(query).unwrap();
-
